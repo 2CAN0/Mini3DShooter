f5c9b746fe6dea77cc636bdb617b3fefd64438c1
2CAN0 <tucaniruito@gmail.com>
2018-12-17 20:19:58 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3(100f, 400f, 100f);
+    public Rigidbody rb;
+    public Transform camera;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private bool spacePressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(-velocity.x * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(velocity.x * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(0, 0, velocity.z * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(0, 0, -velocity.z * Time.deltaTime);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            spacePressed = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !spacePressed)
+        {
+            rb.AddForce(0, velocity.y * Time.deltaTime, 0);
+            spacePressed = true;
+        }
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speedH);
+    }
+}
---
+++ Assets/Scripts/AutoFollowPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offsetThird = new Vector3(0, 1, -5);
+    public float turnSpeed = 4.0f;
+    public float FirstPersonTurnSpeed = 10.0f;
+
+    public bool fp = true;
+
+    private Vector3 offsetPlayer = new Vector3(0, 0, 0);
+    private Vector3 offsetFirst = new Vector3(0, 0.4f, 0);
+
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            if (fp)
+            {
+                fp = false;
+            }
+            else
+            {
+                fp = true;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!fp)
+        {
+            offsetThird = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offsetThird;
+            transform.position = player.position + offsetThird;
+            transform.LookAt(player.position);
+            player.Rotate(0, Input.GetAxis("Mouse X") * turnSpeed, 0);
+        }
+        else
+        {
+            transform.position = player.position + offsetFirst;
+       
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * FirstPersonTurnSpeed);
+            //transform.rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up);
+            //player.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * FirstPersonTurnSpeed);
+            //player.rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up);
+            //player.Translate(transform.TransformDirection(offsetFirst));
+        }
+    }
+}
